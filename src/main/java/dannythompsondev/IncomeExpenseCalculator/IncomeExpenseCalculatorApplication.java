3f0ca8c9303fcd5bd730430cdb1c2c49a2aa42b2
dannythompson901 <dannythompson901@gmail.com>
2019-08-17 11:16:20 -0500

    hello world files.

---
+++ src/main/java/dannythompsondev/IncomeExpenseCalculator/IncomeExpenseCalculatorApplication.java
@@ -0,0 +1,13 @@
+package dannythompsondev.IncomeExpenseCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeExpenseCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomeExpenseCalculatorApplication.class, args);
+	}
+
+}
